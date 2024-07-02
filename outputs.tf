a35e2fa8c0381f45d633c16ec6aabed813b31c79
kevin-loehfelm <kevin.loehfelm@hashicorp.com>
2024-07-02 09:55:30 -0400

    updated default output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "status" {
-  value = "This workspace is using Vault-backed Azure dynamic credentials. Azure Service Principal [${data.azuread_service_principal.current.display_name}]"
+output "azure_tenant_id" {
+  value = data.azuread_client_config.current.tenant_id
+}
+
+output "application_name" {
+  value = data.azuread_service_principal.current.display_name
+}
+
+output "application_client_id" {
+  value = data.azuread_service_principal.current.client_id
 }
